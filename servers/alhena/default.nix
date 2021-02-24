716404127b8a6b7e0f5b097b17011cd62ea5a9ee
Christian Höppner <christian.hoppner@serokell.io>
2021-02-24 16:13:19 +0000

    [OPS-1113] Add server Alhena for Hermetic

---
+++ servers/alhena/default.nix
@@ -0,0 +1,31 @@
+{ modulesPath, inputs, config, pkgs, ... }:
+let
+  vs = config.vault-secrets.secrets;
+in {
+  imports = [
+    inputs.serokell-nix.nixosModules.ec2
+    inputs.hermetic.nixosModules.hermetic
+  ];
+
+  networking.firewall.allowedTCPPorts = [ 80 443 9100 ];
+
+  vault-secrets.secrets.hermetic.environmentVariableNamePrefix = "hermetic";
+  services.hermetic = {
+    enable = true;
+    environmentFile = "${vs.hermetic}/environment";
+    package = inputs.hermetic.defaultPackage.${pkgs.stdenv.system};
+  };
+
+  services.nginx = {
+    enable = true;
+    virtualHosts.hermetic = {
+      default = true;
+
+      serverName = with config.networking; "${hostName}.${domain}";
+      enableACME = true;
+      forceSSL = true;
+    };
+  };
+
+  networking.hostName = "alhena";
+}
