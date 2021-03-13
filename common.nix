93ed345f241f3ce4d6efcabca1b2e4012a18a41c
Alexander Bantyev <balsoft@balsoft.ru>
2021-03-13 23:46:41 +0300

    Use vault-secrets repo and scripts

--- common.nix
+++ common.nix
@@ -2,7 +2,7 @@
   imports = [
     inputs.serokell-nix.nixosModules.common
     inputs.serokell-nix.nixosModules.serokell-users
-    inputs.serokell-nix.nixosModules.vault-secrets
+    inputs.vault-secrets.nixosModules.vault-secrets
   ];
 
   networking.domain = "gemini.serokell.team";
@@ -10,9 +10,8 @@
   systemd.services.amazon-init.enable = false;
 
   vault-secrets = {
-    vaultPathPrefix = "kv/sys/gemini";
+    vaultPrefix = "kv/sys/gemini/${config.networking.hostName}";
     vaultAddress = "https://vault.serokell.org:8200";
-    namespace = config.networking.hostName;
     approlePrefix = "gemini-${config.networking.hostName}";
   };
 }
--- flake.nix
+++ flake.nix
@@ -10,9 +10,11 @@
     };
     deploy-rs.url = "github:serokell/deploy-rs";
     flake-utils.url = "github:numtide/flake-utils";
+    vault-secrets.url = "github:serokell/vault-secrets";
   };
 
-  outputs = { self, nixpkgs, serokell-nix, deploy-rs, flake-utils, ... }@inputs:
+  outputs = { self, nixpkgs, serokell-nix, deploy-rs, flake-utils, vault-secrets
+    , ... }@inputs:
     let
       inherit (nixpkgs.lib) nixosSystem filterAttrs const recursiveUpdate;
       inherit (builtins) readDir mapAttrs;
@@ -34,29 +36,37 @@
 
       overlay = import ./packages;
 
-      deploy.magicRollback = true;
-      deploy.autoRollback = true;
-
-      deploy.nodes = mapAttrs (_: nixosConfig: {
-        hostname =
-          "${nixosConfig.config.networking.hostName}.${nixosConfig.config.networking.domain}";
+      deploy = {
+        magicRollback = true;
+        autoRollback = true;
         sshOpts = [ "-p" "17788" ];
+        nodes = mapAttrs (_: nixosConfig: {
+          hostname =
+            "${nixosConfig.config.networking.hostName}.${nixosConfig.config.networking.domain}";
 
-        profiles.system.user = "root";
-        profiles.system.path =
-          deploy-rs.lib.${system}.activate.nixos nixosConfig;
-      }) self.nixosConfigurations;
+          profiles.system.user = "root";
+          profiles.system.path =
+            deploy-rs.lib.${system}.activate.nixos nixosConfig;
+        }) self.nixosConfigurations;
+      };
     } // flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let pkgs = nixpkgs.legacyPackages.${system}.extend serokell-nix.overlay;
+      let
+        pkgs = serokell-nix.lib.pkgsWith nixpkgs.legacyPackages.${system} [
+          serokell-nix.overlay
+          vault-secrets.overlay
+        ];
       in {
 
-        packages = {
-          inherit (pkgs.extend self.overlay) mtg;
-        };
+        packages = { inherit (pkgs.extend self.overlay) mtg; };
 
         devShell = pkgs.mkShell {
+          VAULT_ADDR = "https://vault.serokell.org:8200";
+          SSH_OPTS = "${builtins.concatStringsSep " " self.deploy.sshOpts}";
           buildInputs = [
             deploy-rs.packages.${system}.deploy-rs
+            pkgs.vault
+            (pkgs.vault-push-approle-envs self)
+            (pkgs.vault-push-approles self)
             (terraformFor pkgs)
             pkgs.nixUnstable
           ];
