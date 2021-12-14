b919fb2989a7a032044ac5deee6b8d4cba175336
notgne2 <gen2@gen2.space>
2021-12-14 03:01:52 -0700

    [OPS-1277] Unify overlays, move suitecrm to a package in the flake

--- servers/pollux/default.nix
+++ servers/pollux/default.nix
@@ -8,48 +8,15 @@
 
   networking.firewall.allowedTCPPorts = [ 80 443 ];
 
-  nixpkgs.overlays = [ inputs.composition-c4.overlay ];
-
   services.nginx.virtualHosts.suitecrm = {
-      serverName = "suitecrm.serokell.team";
-      default = true;
+    serverName = "suitecrm.serokell.team";
+    default = true;
 
-      enableACME = true;
-      forceSSL = true;
-    };
+    enableACME = true;
+    forceSSL = true;
+  };
 
-  services.suitecrm =
-    { enable = true;
-      suitecrmPackage = with pkgs; stdenv.mkDerivation rec {
-        pname = "SuiteCRM";
-        version = "7.12.1";
-
-        src = fetchFromGitHub {
-          owner = "salesagility";
-          repo = "SuiteCRM";
-          rev = "v" + version;
-          sha256 = "sha256-7Zbr+5COkanJd9CLOtX11dz4i/enhFW9FrrA2i8sw4E=";
-        };
-
-        composerDeps = c4.fetchComposerDeps {
-          inherit src;
-        };
-
-        nativeBuildInputs = [
-          php.packages.composer
-          c4.composerSetupHook
-        ];
-
-        installPhase = ''
-          runHook preInstall
-
-          composer install --no-scripts
-          cp -r . $out
-
-          runHook postInstall
-        '';
-      };
-    };
+  services.suitecrm.enable = true;
 
   networking.hostName = "pollux";
   wireguard-ip-address = "172.21.0.33";
