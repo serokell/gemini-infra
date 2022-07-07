ec4648c0c6294360f3757541e79dcfb1f3d7b870
cab <cab404@mailbox.org>
2022-07-07 15:08:28 +0400

    added ligo-webide, changed shell nix to 2.7

--- servers/tejat-prior/default.nix
+++ servers/tejat-prior/default.nix
@@ -10,6 +10,34 @@ with lib;
       allowedUDPPorts = [ config.services.murmur.port ];
     };
 
+
+  containers.ligo-webide-thing = {
+    autoStart = true;
+    config = {
+      imports = [ inputs.ligo-webide.nixosModules.default ];
+      services.ligo-webide.enable = true;
+      services.ligo-webide-frontend = {
+        serverName = "localhost";
+        enable = true;
+      };
+    };
+    ephemeral = true;
+    privateNetwork = true;
+    hostAddress = "192.168.100.10";
+    localAddress = "192.168.100.11";
+  };
+
+  services.nginx = {
+    enable = true;
+    openFirewall = true;
+    addSecurityHeaders = false;
+    virtualHosts."ligo-webide.serokell.team" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/".proxyPass = "http://192.168.100.11:80";
+    };
+  };
+
   services.murmur =
     { enable = true;
       welcometext = "Welcome to the SRE Serokell Mumble server, enjoy your stay!";
