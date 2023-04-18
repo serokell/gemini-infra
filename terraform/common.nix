5e19296e503bf56df468841f3483505204d31454
Sergey Gulin <sergeygulin96@gmail.com>
2023-04-19 01:21:05 +0300

    [Chore] Refactor aws instances and hcloud servers
    Problem: We have added the mkAWS and mkHcloud to terranix-simple, so
    now we can reduce amount of boilerplate when defining aws instances
    and hcloud servers.
    
    Solution: Use mkAWS and mkHcloud to refactor aws instances and hcloud servers.

--- terraform/common.nix
+++ terraform/common.nix
@@ -1,35 +1,16 @@
-{
-  mkAWS = let
-    security-group = [
-      "\${aws_security_group.egress_all.id}"
-      "\${aws_security_group.http.id}"
-      "\${aws_security_group.ssh.id}"
-      "\${aws_security_group.wireguard.id}"
-    ]; in {key_name, volume_size, tags, vpc_security_group_ids ? security-group}: {
-      # Networking
-      availability_zone = "\${module.vpc.azs[2]}";
-      subnet_id = "\${module.vpc.public_subnets[2]}";
-      associate_public_ip_address = true;
-      inherit key_name tags vpc_security_group_ids;
+{ pkgs, ... }:
+let
+  inherit (pkgs.lib) mkAWS mkHcloud;
+in {
+  mkAWS = {key_name, volume_size, tags, vpc_security_group_ids ? null}@args: mkAWS ({
+    availability_zone = "\${module.vpc.azs[2]}";
+    subnet_id = "\${module.vpc.public_subnets[2]}";
+    instance_type = "t3a.micro";
+  } // args);
 
-      # Instance parameters
-      instance_type = "t3a.micro";
-      monitoring = true;
-
-      # Disk type, size, and contents
-      lifecycle.ignore_changes = [ "ami" ];
-      ami = "\${data.aws_ami.nixos.id}";
-      root_block_device = {
-        volume_type = "gp2";
-        inherit volume_size;
-      };
-    };
-
-  mkHcloud = {name, ssh_keys}: {
+  mkHcloud = {name, ssh_keys}: mkHcloud {
     inherit name ssh_keys;
-    image = "ubuntu-20.04";
     server_type = "cx11";
-    lifecycle.ignore_changes = [ "user_data" ];
     # Install NixOS 20.09
     user_data = ''
     #cloud-config
